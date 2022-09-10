d85698f75b21fe4958b69b8a57cf3a182467e77f
joseMarciano <marcianojosepaulo@gmail.com>
2022-09-10 15:51:10 -0300

    style: :gem: put margin bottom

--- src/Header/index.tsx
+++ src/Header/index.tsx
@@ -6,7 +6,7 @@ import { GoPackage } from "react-icons/go"
 
 export function Header(): ReactElement {
 
-    return <Flex align={'center'} justify={'center'}  p={5} as="header">
+    return <Flex mb={10} align={'center'} justify={'center'}  p={5} as="header">
         <Heading display={'flex'} alignItems={'center'} justifyContent={'space-between'}>Order manager <Icon ml={3} as={GoPackage} /></Heading>
     </Flex>
 }
