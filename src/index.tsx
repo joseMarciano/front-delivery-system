a0841c36793531c70ba0080fdc634620a972ee2e
joseMarciano <marcianojosepaulo@gmail.com>
2022-09-12 22:52:55 -0300

    style: :gem: configure styles for lib map

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
+import 'leaflet/dist/leaflet.css'
+import './assets/styles/leaflet.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ChakraProvider, Flex, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/react'
 import { Header } from './Header'
 import { Content } from './Content'
 
